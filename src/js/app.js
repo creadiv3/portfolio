395f8820fd6a65b5ce55392fef095c4a2ec6bdf1
Gary Host <creadiv3@gmail.com>
2022-08-01 12:13:43 +0200

    Add navigation and side navigation

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
+const toggle = document.getElementById('toggle');
+const nav = document.getElementById('nav');
+const sideNav = document.querySelector('.side-nav');
 
+toggle.addEventListener('click', () => {
+  nav.classList.toggle('active');
+  sideNav.classList.toggle('active');
+  toggle.style.zIndex = '4';
+});
