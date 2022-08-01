baccba44786f28c1acaad1501ebaf98d498fceb8
Gary Host <creadiv3@gmail.com>
2022-08-01 21:10:54 +0200

    Add hero section and put some animation on it

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,29 @@
 const toggle = document.getElementById('toggle');
 const nav = document.getElementById('nav');
 const sideNav = document.querySelector('.side-nav');
+const textEl = document.querySelector('.introduce__text');
+const text = 'BARTOSZ KOZŁOWSKI';
+let speed = 80;
+let idx = 0;
+
+// Show/hide side bar menu
 
 toggle.addEventListener('click', () => {
   nav.classList.toggle('active');
   sideNav.classList.toggle('active');
-  toggle.style.zIndex = '4';
+  toggle.style.zIndex = '9999';
 });
+
+// Set autotext on introduce myslef
+
+const writeText = function () {
+  textEl.innerText = text.slice(0, idx);
+
+  idx++;
+  const myTimeout = setTimeout(writeText, speed);
+
+  if (idx === text.length + 1) {
+    clearTimeout(myTimeout);
+  }
+};
+writeText();
